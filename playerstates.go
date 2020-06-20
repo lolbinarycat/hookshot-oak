f3aaa333977774ba5f6428b4d969a264c954183c
binarycat <dogedoge61@gmail.com>
2020-06-20 13:40:07 -0400

    splitting up main.go into multiple files: 1. playerstates.go

---
+++ playerstates.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"time"
+
+	"github.com/oakmound/oak"
+)
+
+
+func (p *Player) AirState() { //start in air state
+
+	if player.PhysObject.ActiColls.GroundHit {
+		p.SetState(p.GroundState)
+		return
+	} else {
+		if p.Mods.WallJump.Equipped && p.ActiColls.HLabel != NoWallJump{
+			if p.PhysObject.ActiColls.LeftWallHit {
+				p.SetState(p.WallSlideLeftState)
+			} else if p.PhysObject.ActiColls.RightWallHit {
+				p.SetState(p.WallSlideRightState)
+			}
+		}
+
+		p.DoGravity()
+	}
+
+	p.DoAirControls()
+}
+
+func (p *Player) GroundState() {
+
+	if player.PhysObject.ActiColls.GroundHit == true {
+		if isJumpInput() {
+			p.Jump()
+		}
+
+	} else {
+		p.SetState(player.CoyoteState)
+	}
+
+	if oak.IsDown(currentControls.Left) {
+		player.Body.Delta.SetX(-player.Body.Speed.X())
+	} else if oak.IsDown(currentControls.Right) {
+		player.Body.Delta.SetX(player.Body.Speed.X())
+	} else {
+		player.Body.Delta.SetX(0)
+		//player.Body.Delta.X()/2)
+	}
+
+}
+
+//the function JumpHeightDecState is the state that decides the height of the players jump.
+//it does this by decreasing the gravity temporaraly when jump is held.
+func (p *Player) JumpHeightDecState() {
+	if p.TimeFromStateStart() > JumpHeightDecTime {
+		p.SetState(p.AirState)
+		return
+	}
+	if oak.IsDown(currentControls.Jump) {
+		//p.DoCustomGravity(Gravity/5)
+	} else {
+		p.DoGravity()
+	}
+	p.DoAirControls()
+}
+
+//CoyoteState implements "coyote time" a window of time after
+//running off an edge in which you can still jump
+func (p *Player) CoyoteState() {
+	if p.StateStartTime.Add(CoyoteTime).Before(time.Now()) {
+		p.SetState(p.AirState)
+	}
+	//inherit code from AirState
+	//p.AirState()
+	if isJumpInput() {
+		p.Jump()
+	}
+
+}
+
+func (p *Player) WallSlideLeftState() {
+	if isJumpInput() {
+		p.WallJump(Right, true)
+		return
+	}
+	if oak.IsDown(currentControls.Climb) && p.Mods.Climb.Equipped {
+		p.SetState(p.ClimbLeftState)
+		return
+	}
+	if p.ActiColls.LeftWallHit == false {
+		p.SetState(p.AirState)
+		return
+	}
+	p.AirState()
+}
+
+func (p *Player) WallSlideRightState() {
+	if isJumpInput() {
+		p.WallJump(Left, true)
+		return
+	}
+	if oak.IsDown(currentControls.Climb) && p.Mods.Climb.Equipped {
+		p.SetState(p.ClimbRightState)
+		return //return to stop airstate for overwriting our change
+	}
+	if p.ActiColls.RightWallHit == false {
+		p.SetState(p.AirState)
+		return
+	}
+	p.AirState()
+}
+
+//func WallJumpLaunchState is entered after you walljump,
+//temporaraly disabling your controls. This should prevent one sided
+//walljumps
+func (p *Player) WallJumpLaunchState() {
+	if p.TimeFromStateStart() >= WallJumpLaunchDuration {
+		p.SetState(p.AirState)
+		return
+	}
+	p.DoGravity()
+}
