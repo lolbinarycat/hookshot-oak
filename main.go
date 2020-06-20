d782bd7e4f8d6ed0b3854e9270eabdfb0239aedb
binarycat <dogedoge61@gmail.com>
2020-06-20 13:03:31 -0400

    reorganized code into subpackage level

--- main.go
+++ main.go
@@ -22,6 +22,9 @@ import (
 	"github.com/oakmound/oak/physics"
 	"github.com/oakmound/oak/render"
 	"github.com/oakmound/oak/scene"
+
+	"github.com/lolbinarycat/hookshot-oak/level"
+	//"main/level"
 )
 
 const (
@@ -36,11 +39,8 @@ var SolidLabels []collision.Label= []collision.Label{
 	NoWallJump,
 }
 
-//color constants
-var (
-	Gray color.RGBA = color.RGBA{100, 100, 100, 255}
-	DullRed color.RGBA = color.RGBA{100,10,10,255}
-)
+
+
 
 const JumpHeight int = 6
 const WallJumpHeight float64 = 6
@@ -519,28 +519,7 @@ func loadScene() {
 	render.Draw(player.Body.R)
 	player.Body.Speed = physics.NewVector(3, float64(JumpHeight))
 
-	ground := entities.NewSolid(10, 400, 500, 20,
-		render.NewColorBox(500, 20, Gray),
-		nil, 0)
-	ground2 := entities.NewSolid(40, 200, 20, 500,
-		render.NewColorBox(20, 500, Gray),
-		nil, 1)
-	ground3 := entities.NewSolid(300, 200, 20, 500,
-		render.NewColorBox(20, 500, DullRed),
-		nil, 2)
-	checkpoint := entities.NewSolid(200,350, 10,10,
-		render.NewColorBox(10,10,color.RGBA{0,0,255,255}),
-		nil,3)
-
-	ground.UpdateLabel(Ground)
-	ground2.UpdateLabel(Ground)
-	ground3.UpdateLabel(Death)
-	checkpoint.UpdateLabel(Checkpoint)
-
-	render.Draw(ground.R)
-	render.Draw(ground2.R, 1)
-	render.Draw(ground3.R, 1)
-	render.Draw(checkpoint.R, 1)
+	level.LoadDevRoom()
 
 	//Give player walljump for now
 	player.Mods.WallJump.Equipped = true
