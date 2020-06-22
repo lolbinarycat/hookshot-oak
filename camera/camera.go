82d0150f1c33dcbfab8def89786d98551a72f951
binarycat <dogedoge61@gmail.com>
2020-06-22 08:51:06 -0400

    used code from examles/rooms to change camera handling

--- camera/camera.go
+++ camera/camera.go
@@ -1,12 +1,13 @@
 package camera
 
 import (
-	"math"
+	//"math"
 	"time"
 
-	"github.com/oakmound/oak/v2"
+	oak "github.com/oakmound/oak/v2"
+	"github.com/oakmound/oak/v2/alg/intgeom"
 	"github.com/oakmound/oak/v2/entities"
-	"github.com/oakmound/oak/v2/key"
+	//"github.com/oakmound/oak/v2/key"
 )
 
 const (
@@ -14,21 +15,27 @@ const (
 	WindowHeight int = 600
 )
 
+const transitionFrameCount = 20
 
 func StartCameraLoop(playerBody *entities.Moving) {
 	camTicker := time.NewTicker(time.Millisecond * 100)
-	go cameraLoop(*camTicker,playerBody)
+	go cameraLoop(*camTicker, playerBody)
 }
 
-func cameraLoop(tick time.Ticker,playerBody *entities.Moving ) {
-	camPosX := 0
-	camPosY := 0
-	playerWidth := playerBody.W
+func cameraLoop(tick time.Ticker, playerBody *entities.Moving) {
+	//camPosX := 0
+	//camPosY := 0
+	//playerWidth := playerBody.W
 	//playerHeight := playerBody.H
-	for {
-		<-tick.C
 
-		camPosX = int(math.Floor((playerBody.X()+playerWidth/2)/float64(WindowWidth)))
+	var transitioning bool
+	var totalTransitionDelta intgeom.Point2
+	var transitionDelta intgeom.Point2
+
+	for {
+		//<-tick.C
+
+		//camPosX = int(math.Floor((playerBody.X()+playerWidth/2)/float64(WindowWidth)))
 		//oak.SetScreen(int(player.Body.X()),int(player.Body.Y()))
 		// if int(playerBody.X())/WindowWidth < camPosX+1 {
 		// 	camPosX--
@@ -42,12 +49,49 @@ func cameraLoop(tick time.Ticker,playerBody *entities.Moving ) {
 		// } else {
 		//	continue //if no camera position change occured, don't update the screen positon
 		//}
-		oak.SetScreen(WindowWidth*camPosX, WindowHeight*camPosY)
+		/*oak.SetScreen(WindowWidth*camPosX, WindowHeight*camPosY)
 
 		oak.SetViewportBounds(camPosX*WindowWidth,camPosX*WindowHeight,
 			camPosX*WindowWidth+WindowHeight,camPosX*WindowHeight+WindowHeight )
 		if oak.IsDown(key.RightShift) {
 			oak.SetScreen(int(playerBody.X())-WindowWidth/2,0)
+		}*/
+
+		dir, ok := isOffScreen(playerBody)
+		if !transitioning && ok {
+			transitioning = true
+			totalTransitionDelta = intgeom.Point2{oak.ScreenWidth, oak.ScreenHeight}.Mul(intgeom.Point2{dir.X(), dir.Y()})
+			transitionDelta = totalTransitionDelta.DivConst(transitionFrameCount)
 		}
+		if transitioning {
+			// disable movement
+			// move camera one size towards the player
+			if totalTransitionDelta.X() != 0 || totalTransitionDelta.Y() != 0 {
+				oak.ShiftScreen(transitionDelta.X(), transitionDelta.Y())
+				totalTransitionDelta = totalTransitionDelta.Sub(transitionDelta)
+			} else {
+				transitioning = false
+			}
+
+		}
+
 	}
+}
+
+func isOffScreen(char *entities.Moving) (intgeom.Dir2, bool) {
+	x := int(char.X())
+	y := int(char.Y())
+	if x > oak.ViewPos.X+oak.ScreenWidth {
+		return intgeom.Right, true
+	}
+	if y > oak.ViewPos.Y+oak.ScreenHeight {
+		return intgeom.Down, true
+	}
+	if x+int(char.W) < oak.ViewPos.X {
+		return intgeom.Left, true
+	}
+	if y+int(char.H) < oak.ViewPos.Y {
+		return intgeom.Up, true
+	}
+	return intgeom.Dir2{}, false
 }
