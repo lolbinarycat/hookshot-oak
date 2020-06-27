6937e4f9aba8701c8fe8468a79d95c9b42cbb534
binarycat <dogedoge61@gmail.com>
2020-06-27 19:14:39 -0400

    collition is now checked more than once per frame, making it harder to clip through things

--- main.go
+++ main.go
@@ -290,44 +290,7 @@ func (o *PhysObject) DoCustomGravity(grav float64) {
 	o.Body.Delta.ShiftY(grav)
 }
 
-func (object *PhysObject) DoCollision(updater func()) {
-	_, oldY := object.Body.GetPos()
-	updater()
-	object.ActiColls = ActiveCollisions{} //reset the struct to be all false
 
-	object.Body.ShiftX(object.Body.Delta.X())
-	hit := collision.HitLabel(object.Body.Space,
-		append(labels.Solids, object.ExtraSolids...)...)
-	if hit != nil {
-		if object.Body.Delta.X() > 0 { //Right Wall
-			object.ActiColls.RightWallHit = true
-			object.Body.SetX(hit.X() - object.Body.W)
-		} else if object.Body.Delta.X() < 0 { //Left Wall
-			object.ActiColls.LeftWallHit = true
-			object.Body.SetX(hit.X() + hit.W())
-		}
-		object.Body.Delta.SetX(0)
-		object.ActiColls.HLabel = hit.Label
-		object.ActiColls.LastHitH = hit.CID
-	}
-
-	object.Body.ShiftY(object.Body.Delta.Y())
-	if hit := collision.HitLabel(object.Body.Space,
-		append(object.ExtraSolids, labels.Solids...)...); hit != nil {
-		if object.Body.Delta.Y() > 0 { //Ground
-			object.ActiColls.GroundHit = true
-			object.Body.SetY(hit.Y() - object.Body.H)
-		} else if object.Body.Delta.Y() < 0 { //Ceiling
-			object.ActiColls.CeilingHit = true
-			//TODO: make this work like other collision
-			object.Body.SetY(oldY)
-		}
-		object.Body.Delta.SetY(0)
-		object.ActiColls.VLabel = hit.Label
-		object.ActiColls.LastHitV = hit.CID
-	}
-
-}
 func openFileAsBytes(filename string) ([]byte, error) {
 	dlog.Info("opening file", filename)
 	file, err := os.Open(filename)
