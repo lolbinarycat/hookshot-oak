3614a41adfbdc40928212e65e081dd99b5cc005f
binarycat <dogedoge61@gmail.com>
2020-07-17 18:39:20 -0400

    added dirt

--- assets/tileset.tsx
+++ assets/tileset.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.1" name="tileset" tilewidth="8" tileheight="8" tilecount="3" columns="0">
+<tileset version="1.4" tiledversion="1.4.1" name="tileset" tilewidth="8" tileheight="8" tilecount="4" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0" type="ground">
   <properties>
@@ -12,5 +12,8 @@
  </tile>
  <tile id="2" type="checkpoint">
   <image width="8" height="8" source="images/checkpoint.png"/>
+ </tile>
+ <tile id="3" type="dirt">
+  <image width="8" height="8" source="images/dirt.png"/>
  </tile>
 </tileset>
