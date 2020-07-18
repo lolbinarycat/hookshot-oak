4b8f05a3618b77cf95c7aff39c4a874ee33d19cd
binarycat <dogedoge61@gmail.com>
2020-07-18 12:39:19 -0400

    more old code removed

--- level/level.go
+++ level/level.go
@@ -92,24 +92,7 @@ func OpenFileAsBytes(filename string) ([]byte, error) {
 func LoadDevRoom() error {
 	n := newCounter(3)
 	fmt.Println(n())
-	// ground := entities.NewSolid(10, 400, 500, 20,
-	// 	render.NewColorBox(500, 20, Gray),
-	// 	nil, n())
-	// ground.Init()
 
-	//wallSprite, err := render.LoadSprite("assets/images","wall.png")
-
-	//dlog.ErrorCheck(err)
-	//wallSprite.Modify(mod.Scale(2,2))
-
-	// wall1 := entities.NewSolid(40, 200, 20, 500,
-	// 	render.NewColorBox(20, 500, Gray),
-	// 	nil, n())
-	// wall1.Init()
-	// wall2 := entities.NewSolid(300, 200, 20, 500,
-	// 	render.NewColorBox(20, 500, Gray),
-	// 	nil, n())
-	// wall2.Init()
 	checkpoint := entities.NewSolid(200, 350, 10, 10,
 		render.NewColorBox(10, 10, color.RGBA{0, 0, 255, 255}),
 		nil, n())
@@ -126,128 +109,29 @@ func LoadDevRoom() error {
 	checkpoint.UpdateLabel(labels.Checkpoint)
 	death.UpdateLabel(labels.Death)
 
-	//render.Draw(ground.R)
-	//render.Draw(wall1.R, 1)
-	//render.Draw(wall2.R, 1)
+
 	render.Draw(checkpoint.R, 1)
 	render.Draw(death.R)
 
-	//LoadJsonLevelData("level.json",-800,0)
+
 
 	err := LoadTmx("assets/level.tmx")
 	if err != nil {
 		return err
 	}
 
-	//err := loadSvg("level.svg", -800, 0)
-	//dlog.ErrorCheck(err)
+
 	return nil
 }
 
-// func loadSvg(filename string, offsetX, offsetY float64) error {
-
-// 	dlog.Info("opening file", filename)
-// 	file, err := os.Open(filename)
-// 	defer file.Close()
-// 	if err != nil {
-// 		return err
-// 	}
-// 	/*fileInfo, err := file.Stat()
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	fileSize := fileInfo.Size()*/
-
-// 	/*
-
-// 	svgData, err := svg.ParseSvgFromReader(reader, filename, 1)
-// 	*/
-// 	reader := bufio.NewReader(file)
-// 	decoder := xml.NewDecoder(reader)
-// 	//svgBytes, err := OpenFileAsBytes(filename)
-// 	//if err != nil {
-// 	//	return err
-// 	//}
-	
-// 	rects := make([]xml.Rect,2)
-// 	decoder.Decode(&rects)
-// 	//xml.Unmarshal(svgBytes,&rects)
-// 	/*{
-// 		f := strconv.ParseFloat
-// 		for i, item := range svgData.Elements {
-// 			item = item.(svg.Rect)
-// 			entities.NewSolid(f(item.Rx, 64), f(item.R, 64),
-// 				f(item.Width), f(item.Height),
-// 				render.NewColorBox(f(item.Width), f(item.Height),
-// 					color.RGBA{100, 100, 100}),
-// 				nil, i+200)
-// 		}
-// 	}*/
-// 	fmt.Println(rects[1])
-// 	return nil
-// }
-
-// ~~level data is to be stored as json, problebly compressed in the final game~~
-// obsolete, do not use
-func LoadJsonLevelData(filename string,offsetX,offsetY float64) {
-	dlog.Info("loading json level data from", filename)
-	file, err := os.Open(filename)
-	if err != nil {
-		//t.Print("error when opening screen file: ")
-		//panic(err)
-		dlog.Error("error when opening screen file",err)
-		return
-	}
-	fileInfo, err := file.Stat()
-	if err != nil {
-		fmt.Print("error when getting file info: ")
-		panic(err)
-	}
-	fileSize := fileInfo.Size()
-	reader := bufio.NewReader(file)
-	var rawJson []byte = make([]byte, int(fileSize))
-	_, err = reader.Read(rawJson)
-	if err != nil {
-		fmt.Print("error when reading file into byte array: ")
-		panic(err)
-	}
-	var screenData JsonScreen
-	err = json.Unmarshal(rawJson, &screenData)
-	if err != nil {
-		defer fmt.Println("json:", rawJson)
-		fmt.Print("error unmarshaling screen data: ")
-		panic(err)
-	}
-
-	for i, rectData := range screenData.Rects {
-		rect := entities.NewSolid(rectData.X+offsetX, rectData.Y+offsetY, rectData.W, rectData.H,
-			render.NewColorBox(int(rectData.W), int(rectData.H), color.RGBA{100, 100, 100, 255}),
-			nil, event.CID(i+10))
-
-		rect.Init()
-		rect.UpdateLabel(rectData.Label)
-		render.Draw(rect.R)
-	}
-}
 
 func LoadTmx(mapPath string) error {
-	/*mapReader, err := fileutil.Open(mapPath)
-	if err != nil {
-		return err
-	}
 
-	gameMap, err := tiled.LoadFromReader("assets",mapReader)
-	if err != nil {
-		return err
-	}*/
 	levelMap, err := tiled.LoadFromFile(mapPath)
 	if err != nil {
 		return err
 	}
-	fmt.Println(levelMap)
 
-
-	//fmt.Println(levelMap.TileHeight,levelMap.TileWidth)
 	err = LoadTileLayers(levelMap)
 	if err != nil {
 		return err
@@ -257,13 +141,7 @@ func LoadTmx(mapPath string) error {
 	return nil
 }
 
-/*func LoadTmxLayer(layer *tiled.Layer,parentMap *tiled.Map) {
-	for i, tile := layer.Tiles {
-		} else {
-			entities.NewSolid((parentMap.TileWidth*i)%parentMap.Width, y float64, w float64, h float64, r render.Renderable, tree *collision.Tree, cid event.CID)
-		}
-	}
-}*/
+
 
 func LoadTileLayers(levelMap *tiled.Map) error {
 	// for each _Loop, the contents of the loop are ran once for every _
