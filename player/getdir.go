e3243c1a0b0a264797624feaf35faba1c7590018
binarycat <dogedoge61@gmail.com>
2020-07-23 14:05:48 -0400

    more logging

--- player/getdir.go
+++ player/getdir.go
@@ -2,12 +2,15 @@ package player
 
 import "github.com/lolbinarycat/hookshot-oak/direction"
 import "github.com/oakmound/oak/v2"
+import "github.com/oakmound/oak/v2/dlog"
 
 func (c *ControlConfig) GetDir() (dir direction.Dir) {
 	if oak.IsDown(c.Up) {
+		dlog.Verb("Up held")
 		dir = dir.Add(direction.MaxUp())
 	}
 	if oak.IsDown(c.Down) {
+		dlog.Verb("Down held")
 		dir = dir.Add(direction.MaxDown())
 	}
 	if oak.IsDown(c.Left) {
@@ -16,5 +19,6 @@ func (c *ControlConfig) GetDir() (dir direction.Dir) {
 	if oak.IsDown(c.Right) {
 		dir = dir.Add(direction.MaxRight())
 	}
+	dlog.Verb("GetDir returned",dir)
 	return
 }
