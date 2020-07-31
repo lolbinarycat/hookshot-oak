6cd8ee51ddf7b8621f79d665db3c85970e395f54
binarycat <dogedoge61@gmail.com>
2020-07-31 11:26:55 -0400

    conditions

---
+++ player/conditions.go
@@ -0,0 +1,32 @@
+package player
+
+import (
+	"github.com/lolbinarycat/hookshot-oak/player/condition"
+	"github.com/oakmound/oak/v2/dlog"
+)
+
+func (s *PlayerState) DoMap(p *Player) (stateChanged bool) {
+	stateChanged = false
+	for cond, fun := range s.Map {
+		if cond.True(p) {
+			nextState := fun(p)
+			if nextState != nil {
+				dlog.Verb("cond: not nil")
+				p.SetState(*nextState)
+				stateChanged = true
+				break
+			}
+			dlog.Verb("cond is true")
+		}
+	}
+	return stateChanged
+}
+
+func (s *PlayerState) InitMap() {
+	for cond := range s.Map {
+		switch cond.(type) {
+		case condition.Initalized:
+			cond.(condition.Initalized).Init()
+		}
+	}
+}
