949bbe2579eeefe4278e0792113ce1ff622105cf
binarycat <dogedoge61@gmail.com>
2020-08-01 14:04:06 -0400

    cleaned up comments

--- ui/optionlist.go
+++ ui/optionlist.go
@@ -19,8 +19,8 @@ type OptionList struct {
 func NewOptionList(x, y float64, opts ...*Option) *OptionList {
 	ol := OptionList{
 		LayeredPoint: render.NewLayeredPoint(x, y, 0),
-		Options: opts,
-		FocusMarker: render.NewStrText(">", 14, 1),
+		Options:      opts,
+		FocusMarker:  render.NewStrText(">", 14, 1),
 	}
 	return &ol
 }
@@ -31,13 +31,13 @@ func (ol *OptionList) Init() event.CID {
 }
 
 type Option struct {
-	Name string
+	Name   string
 	Action func()
 }
 
 func (l OptionList) String() string {
 	var bldr strings.Builder
-	bldr.Grow(len(l.Options)*20)
+	bldr.Grow(len(l.Options) * 20)
 	for _, opt := range l.Options {
 		bldr.WriteString(opt.Name)
 		bldr.WriteRune('\n')
@@ -47,27 +47,23 @@ func (l OptionList) String() string {
 
 func (l OptionList) DrawOffset(buff draw.Image, xOff, yOff float64) {
 	x, y := xOff+l.X(), yOff+l.Y()
-	var txt = make([]*render.Text,len(l.Options))
+	var txt = make([]*render.Text, len(l.Options))
 	txt[0] = render.NewStrText(l.Options[0].Name, x, y)
-	_ , lineH := txt[0].GetDims()
+	_, lineH := txt[0].GetDims()
 
 	for i := range txt[1:] {
-		// through some witchcraft, y is incremented.
 		txt[i+1] = render.NewStrText(l.Options[i+1].Name, x, y)
 	}
 
-	//txtW, txtH := txt[l.focused].GetDims()
-	l.FocusMarker.DrawOffset(buff,x,
+	l.FocusMarker.DrawOffset(buff, x,
 		y+float64(lineH*(1+l.focused)+lineH/2))
 	for i, ln := range txt {
-		//if ln != nil {
-			ln.DrawOffset(buff, x, y+float64(lineH*i))
-		//}
+		ln.DrawOffset(buff, x, y+float64(lineH*i))
 	}
 }
 
 func (l OptionList) Draw(buff draw.Image) {
-	l.DrawOffset(buff,0,0)
+	l.DrawOffset(buff, 0, 0)
 }
 
 func (l *OptionList) Cycle() {
