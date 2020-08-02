c6b321c48f8dd6d84348e913c1ad01a57fb62780
binarycat <dogedoge61@gmail.com>
2020-08-02 11:04:10 -0400

    titlescreen rework

---
+++ titlescreen.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"os"
+	"github.com/oakmound/oak/v2/scene"
+	"github.com/oakmound/oak/v2/render"
+	"github.com/oakmound/oak/v2/event"
+	"github.com/oakmound/oak/v2/key"
+
+	"github.com/lolbinarycat/hookshot-oak/ui"
+)
+
+var titlescreenScene scene.Scene
+
+// TitlescreenResult is the struct that will be passed to the next scene.
+type TitlescreenResult struct {
+	LoadSave bool
+}
+
+func init() {
+	var optList *ui.OptionList
+	var res TitlescreenResult
+	var startGame bool
+	titlescreenScene = scene.Scene{
+		Start: func(_ string, _ interface{}) {
+			optList = ui.NewOptionList(20,20,[]*ui.Option{
+				{"Continue", func() {
+					res.LoadSave = true
+					startGame = true
+				}},
+				{"New Game", func() {
+					res.LoadSave = false
+					startGame = true
+				}},
+				{"Quit", func () {
+					os.Exit(0)
+				}},
+			}...)
+			optList.Init()
+			render.Draw(optList)
+			event.Bind(func(_ int, k interface{}) int {
+				switch k.(string) {
+				case key.Tab:
+					optList.Cycle()
+				case key.Enter:
+					optList.ActivateSelected()
+				}
+				return 0
+			},key.Down,int(optList.CID))
+		},
+		Loop: func () bool {
+			return !startGame
+		},
+		End: func () (string,*scene.Result) {
+			return mainSceneName, &scene.Result{NextSceneInput:res}
+		},
+	}
+}
