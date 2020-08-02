d55c45b338561554d6d84444bab8e0ab827a9eca
binarycat <dogedoge61@gmail.com>
2020-08-02 16:28:46 -0400

    reworked to be less flexible but more reliable

--- player/renderable/renderable.go
+++ player/renderable/renderable.go
@@ -10,6 +10,17 @@ import (
 	"github.com/oakmound/oak/v2/render"
 )
 
+const PlayerWidth = 12
+const PlayerHeight = 12
+
+const (
+	EyesYStart = PlayerHeight / 6
+	EyesYEnd = EyesYStart * 4
+	EyeGap = 4
+	Eye1PosX = (PlayerWidth / 2) - (EyeGap / 2)
+	Eye2PosX = (PlayerWidth / 2) + (EyeGap / 2)
+)
+
 type PlayerR struct {
 	// we embed a renderable that is used as a base
 	// the position of this renderable is the position of the
@@ -30,34 +41,41 @@ type Eyes struct {
 }
 
 func (e Eyes) At(x, y int) color.Color {
-
-	// innerRect is a rect that contains the eyes.
-	var innerRect image.Rectangle
-	var innerRectBounds = image.Pt(e.Size.Y,(e.Size.X*2+e.Gap))
-
-	innerRect.Max = innerRectBounds
-	// this massive operation centers the rectangle
-	innerRect = innerRect.Add(image.Pt(
-		(e.bounds.X-innerRect.Max.X)/2,
-		(e.bounds.Y-innerRect.Max.X)/2,
-	))
-	innerRect = innerRect.Add(e.Offset)
-
-	inPt := image.Pt(x,y)
-	if inPt.In(innerRect) {
-		// bRect is the rectangle of space between the eyes
-		var bRect image.Rectangle
-		bRect = innerRect
-		bRect.Max.X -= e.Size.X
-		bRect.Min.X += e.Size.X
-		if inPt.In(bRect) {
-			return nil
-		} else {
+	//return e.Color
+	switch x {
+	case e.Offset.X + Eye1PosX, e.Offset.X + Eye2PosX:
+		if y > EyesYStart + e.Offset.Y && y < EyesYEnd + e.Offset.Y {
 			return e.Color
 		}
-	} else {
-		return nil
 	}
+	return nil
+	// // innerRect is a rect that contains the eyes.
+	// var innerRect image.Rectangle
+	// var innerRectBounds = image.Pt(e.Size.Y,(e.Size.X*2+e.Gap))
+
+	// innerRect.Max = innerRectBounds
+	// // this massive operation centers the rectangle
+	// innerRect = innerRect.Add(image.Pt(
+	// 	(e.bounds.X-innerRect.Max.X)/2,
+	// 	(e.bounds.Y-innerRect.Max.X)/2,
+	// ))
+	// innerRect = innerRect.Add(e.Offset)
+
+	// inPt := image.Pt(x,y)
+	// if inPt.In(innerRect) {
+	// 	// bRect is the rectangle of space between the eyes
+	// 	var bRect image.Rectangle
+	// 	bRect = innerRect
+	// 	bRect.Max.X -= e.Size.X
+	// 	bRect.Min.X += e.Size.X
+	// 	if inPt.In(bRect) {
+	// 		return nil
+	// 	} else {
+	// 		return e.Color
+	// 	}
+	// } else {
+	// 	return nil
+	// }
 
 
 }
@@ -79,4 +97,14 @@ func (p PlayerR) DrawOffset(buff draw.Image, xOff, yOff float64) {
 				clr)
 		}
 	}
+}
+
+func (p PlayerR) Draw(buff draw.Image) {
+	p.DrawOffset(buff, 0, 0)
+}
+
+
+func (p *PlayerR) SetEyeOffset(x, y int) {
+	p.Eyes.Offset.X = x
+	p.Eyes.Offset.Y = y
 }
