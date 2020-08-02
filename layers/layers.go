f0a7e219a8ac1118d91dd17172cdb2d824c20279
binarycat <dogedoge61@gmail.com>
2020-08-02 11:26:57 -0400

    added new layers package for management of draw stack layers

---
+++ layers/layers.go
@@ -0,0 +1,28 @@
+// Package layers initializes and indexes the draw stack.
+// It provides both constants describing the indexes of layers,
+// and pointers to the actual layers.
+package layers
+
+import "github.com/oakmound/oak/v2/render"
+
+// Indexes for layers
+const (
+	BG int = iota
+	FG
+	UI
+)
+
+// Actual pointers to layers
+var (
+	BGLayer, 
+	FGLayer,
+	UILayer *render.RenderableHeap
+)
+
+func init() {
+	BGLayer = render.NewDynamicHeap()
+	FGLayer = render.NewDynamicHeap()
+	UILayer = render.NewStaticHeap()
+
+	render.SetDrawStack(BGLayer,FGLayer,UILayer)
+}
